9e74880b4ffabe276176c73d81700cf286150095
Jesús España Sandoval <jesusitoJ1@hotmail.com>
2022-12-29 18:05:16 -0500

    Center the content of the contacts card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,13 @@
   <div class="d-flex justify-content-center">
     @forelse ($contacts as $contact)
       <div class="card text-center mx-2" style="width: 20rem;">
-        <div class="card-body">
+        <div class="card-body d-flex flex-column align-items-center justify-content-center">
           <h3 class="card-title">{{ $contact->name }}</h3>
           <h4 class="card-text">{{ $contact->phone_number }}</h4>
-          <a href="#" class="btn btn-danger me-2">Delete</a>
-          <a href="#" class="btn btn-info">Edit</a>
+          <div>
+            <a href="#" class="btn btn-danger me-2">Delete</a>
+            <a href="#" class="btn btn-info">Edit</a>
+          </div>
         </div>
       </div>
     @empty
